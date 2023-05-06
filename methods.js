a513df135a4f633fc9e0219ccfd5b36dd82ff864
paras9sid <sidharthjainp9@gmail.com>
2023-05-06 22:38:42 +0530

    this keyword

--- methods.js
+++ methods.js
@@ -21,3 +21,23 @@ const myMAth2 = {
     return x ** 3;
   },
 };
+
+//this keyword
+
+const cat = {
+  name: "Blue steele",
+  color: "grey",
+  breed: "scottish",
+  meow() {
+    // console.log("meow meow meow");
+    // console.log(color); // Uncaught ReferenceError: color is not defined
+    //using this
+    console.log(this.color); // grey - color of cat object access now through the method meow
+    console.log(this); // this keyword refers to cat object(its prperties) in here
+  },
+};
+
+//defining in open refereds to main object that is window in below case which cat2 refers to
+const cat2 = cat.meow; //cat2() -?> this.color not printing as this keyword refers to window object here
+
+//try - catch
