c5c6609e3a8528a8d79a3ecef11dee664ce62415
paras9sid <sidharthjainp9@gmail.com>
2023-05-06 21:58:12 +0530

    defining methods and shorthand of methods too

---
+++ methods.js
@@ -0,0 +1,23 @@
+// methods  in js
+
+const myMath = {
+  PI: 3.14519,
+  square: function (num) {
+    return num * num;
+  },
+  cube: function (num) {
+    return num ** 3;
+  },
+};
+
+//shorthand of above method
+
+const myMAth2 = {
+  PI: 3.14,
+  square2(x) {
+    return x * x;
+  },
+  cube2(x) {
+    return x ** 3;
+  },
+};
