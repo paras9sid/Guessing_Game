9eff4a9d1a3e640ce84e4d24656a7a56263e7f20
paras9sid <sidharthjainp9@gmail.com>
2023-05-09 16:36:55 +0530

    forEach implemented

---
+++ loops.js
@@ -0,0 +1,57 @@
+// for each
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function print(element) {
+  console.log(element);
+}
+
+// //individual element print
+// print(numbers[0]); // 1
+// print(numbers[1]); // 2
+
+// //for-each loop
+// numbers.forEach(print); // 1- 9 in new line each
+
+//function
+
+//1
+// numbers.forEach(function (el) {
+//   console.log(el);
+// });
+
+// //above in for loop method
+
+// for (let el of numbers) {
+//   console.log(el);
+// }
+
+//2
+
+// numbers.forEach(function (el) {
+//   //even numbers
+//   if (el % 2 == 0) {
+//     console.log(el);
+//   }
+// });
+
+const movies = [
+  {
+    title: "hello",
+    score: 99,
+  },
+  {
+    title: "new",
+    score: 23,
+  },
+  {
+    title: "para",
+    score: 81,
+  },
+];
+
+//90/100
+
+movies.forEach(function (movie) {
+  console.log(`${movie.title} - ${movie.score}/100`);
+});
--- methods.js
+++ methods.js
@@ -1,43 +1,59 @@
-// methods  in js
+// // methods  in js
 
-const myMath = {
-  PI: 3.14519,
-  square: function (num) {
-    return num * num;
-  },
-  cube: function (num) {
-    return num ** 3;
-  },
-};
+// const myMath = {
+//   PI: 3.14519,
+//   square: function (num) {
+//     return num * num;
+//   },
+//   cube: function (num) {
+//     return num ** 3;
+//   },
+// };
 
-//shorthand of above method
+// //shorthand of above method
 
-const myMAth2 = {
-  PI: 3.14,
-  square2(x) {
-    return x * x;
-  },
-  cube2(x) {
-    return x ** 3;
-  },
-};
+// const myMAth2 = {
+//   PI: 3.14,
+//   square2(x) {
+//     return x * x;
+//   },
+//   cube2(x) {
+//     return x ** 3;
+//   },
+// };
 
-//this keyword
+// //this keyword
 
-const cat = {
-  name: "Blue steele",
-  color: "grey",
-  breed: "scottish",
-  meow() {
-    // console.log("meow meow meow");
-    // console.log(color); // Uncaught ReferenceError: color is not defined
-    //using this
-    console.log(this.color); // grey - color of cat object access now through the method meow
-    console.log(this); // this keyword refers to cat object(its prperties) in here
-  },
-};
+// const cat = {
+//   name: "Blue steele",
+//   color: "grey",
+//   breed: "scottish",
+//   meow() {
+//     // console.log("meow meow meow");
+//     // console.log(color); // Uncaught ReferenceError: color is not defined
+//     //using this
+//     console.log(this.color); // grey - color of cat object access now through the method meow
+//     console.log(this); // this keyword refers to cat object(its prperties) in here
+//   },
+// };
 
-//defining in open refereds to main object that is window in below case which cat2 refers to
-const cat2 = cat.meow; //cat2() -?> this.color not printing as this keyword refers to window object here
+// //defining in open refereds to main object that is window in below case which cat2 refers to
+// const cat2 = cat.meow; //cat2() -?> this.color not printing as this keyword refers to window object here
 
 //try - catch
+//1
+// hello.toUpperCase(); // Uncaught ReferenceError: hello is not defined
+//2
+// try{
+//     hello.toUpperCase(); // Uncaught SyntaxError: Missing catch or finally after try (at methods.js:49:1)
+// }
+
+//3
+
+try {
+  hello.toUpperCase();
+} catch {
+  console.log("Error"); // Error print in console code dont stop comes in catch block
+}
+
+console.log("After try catch stmt ex"); // execurted and prints After try catch stmt ex -- code doesnt stop
