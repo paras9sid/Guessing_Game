068cd18e8c84782bfab577acd40f96f1f8fbff3c
paras9sid <sidharthjainp9@gmail.com>
2023-05-18 15:30:03 +0530

    querySelector done

--- dom.js
+++ dom.js
@@ -14,11 +14,19 @@
 //all images with img tag name will be changed to url in img.src
 //getElementsByClassName
 
-const squareImages = document.getElementsByClassName("square");
+// const squareImages = document.getElementsByClassName("square");
 
-for (let img of squareImages) {
-  img.src =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
-}
+// for (let img of squareImages) {
+//   img.src =
+//     "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg";
+// }
 
-//all images with class anme square changed to url in img.src
+// //all images with class anme square changed to url in img.src
+
+//query selector
+
+// const links = document.querySelectorAll("p a");
+
+// for (let link of links) {
+//   console.log(link.href);
+// }
