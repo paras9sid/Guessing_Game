175610fae7e4c619f9c9bada918324ca0762a92f
paras9sid <sidharthjainp9@gmail.com>
2023-05-28 18:58:03 +0530

    async keyword used , login functionality created.

---
+++ async.js
@@ -0,0 +1,47 @@
+//async kyword used , always return a promise
+
+// async function hello() {}
+
+//1
+// const sing = async () => {}; // Promise {<fulfilled>: undefined}
+
+// //2
+// const sing2 = async () => {
+//   return "LA LA LA LA"; //Promise {<fulfilled>: 'LA LA LA LA'}
+// };
+
+// sing2().then((data) => {
+//   console.log("Promise resolved! ", data);
+// });
+
+//3 with error
+// const sing3 = async () => {
+//   throw "Oh no , problem"; // rejected promise //Promise {<rejected>: 'Oh no , problem'}
+//   return "LA LA LA LA"; //Promise {<fulfilled>: 'LA LA LA LA'}
+// };
+
+// sing3()
+//   .then((data) => {
+//     console.log("Promise resolved! ", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error occurs", err);
+//   });
+
+//4
+const login = async (username, password) => {
+  if (!username || !password) throw "Missing credentials";
+
+  if (password === "hello") return "Welcome!";
+  throw "Invalid password";
+};
+
+login("asdadada", "hello")
+  .then((msg) => {
+    console.log("Logged in");
+    console.log(msg);
+  })
+  .catch((err) => {
+    console.log("Error");
+    console.log(err);
+  });
