64b09931a3a17c47af97e2048c2ec813a8ea94e1
paras9sid <sidharthjainp9@gmail.com>
2023-05-28 21:53:44 +0530

    await with promise request.

--- promise.js
+++ promise.js
@@ -177,3 +177,22 @@ const delayedColorChange = (color, delay) => {
 // }
 
 // printRainbow(); // will excute above function wirtten or else in console haas to call printRainbow(); like this
+
+//converted above callback function -> into Promises
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500 + 500);
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
+
+async function makeTwoRequests() {
+  let data1 = await fakeRequestPromise("/page1");
+  console.log(data1);
+}
