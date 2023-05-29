767194b67ec6fd3b3f54cd7ba635b1a33e9863f5
paras9sid <sidharthjainp9@gmail.com>
2023-05-29 16:49:08 +0530

    jokes api fetch and appended to list in webpage

--- axios.js
+++ axios.js
@@ -4,13 +4,56 @@
 //   .catch((err) => console.log("error! ", err));
 
 // async await conversion
-const starWArs = async (id) => {
+// const starWArs = async (id) => {
+//   try {
+//     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//     console.log(res.data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// starWArs(1);
+// starWArs(2);
+
+// //dad joke api
+
+// const dadJoke = async () => {
+//   try {
+//     //creating headers object
+//     const config = {
+//       headers: {
+//         Accept: "application/json",
+//       },
+//     };
+//     const res = await axios.get("https://icanhazdadjoke.com/", config);
+//     // console.log(res);
+//     console.log(res.data.joke);
+//   } catch (e) {
+//     console.log("Error! ", e);
+//   }
+// };
+
+// dadJoke();
+
+// /dad joke api - appending in html web page ul
+
+const jokes = document.querySelector("#jokes");
+
+const dadJoke = async () => {
   try {
-    const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-    console.log(res.data);
-  } catch (err) {
-    console.log(err);
+    //creating headers object
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    // console.log(res);
+    console.log(res.data.joke);
+    const newLi = document.createElement("li");
+    newLi.append(res.data.joke);
+    jokes.append(newLi);
+  } catch (e) {
+    console.log("Error! ", e);
   }
 };
-starWArs(1);
-starWArs(2);
